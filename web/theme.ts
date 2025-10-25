93628f7b958b4f47ef6d09df0bc7048119d11c07
GibbysGoods <jriehl728@gmail.com>
2025-10-25 00:38:56 +0000

    Delete web/theme.ts

--- web/theme.ts
+++
@@ -1,3 +0,0 @@
-import { createTheme } from "@mui/material";
-export const light = createTheme({ palette: { mode: "light" }, shape: { borderRadius: 16 } });
-export const dark = createTheme({ palette: { mode: "dark" }, shape: { borderRadius: 16 } });
