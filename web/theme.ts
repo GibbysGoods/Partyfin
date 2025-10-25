50b17c702cdb235b69932f96d088bad1917ffb45
GibbysGoods <jriehl728@gmail.com>
2025-10-25 00:37:21 +0000

    Create theme.ts

---
+++ web/theme.ts
@@ -0,0 +1,3 @@
+import { createTheme } from "@mui/material";
+export const light = createTheme({ palette: { mode: "light" }, shape: { borderRadius: 16 } });
+export const dark = createTheme({ palette: { mode: "dark" }, shape: { borderRadius: 16 } });
